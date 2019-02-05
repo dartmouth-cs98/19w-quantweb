1eb8a216d496273d646f040ef6df5633e00484de
Tyler <tyler@burnamtech.com>
2019-02-05 14:06:16 -0500

    hooked up auth to forms

--- src/containers/RegisterUserContainer.js
+++ src/containers/RegisterUserContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { signupUser } from '../actions';
 import WrappedRegisterUserForm from './RegisterUserForm';
 
 class RegisterUserContainer extends React.Component {
@@ -8,19 +9,29 @@ class RegisterUserContainer extends React.Component {
     super(props);
 
     this.state = {};
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+
+  // Call sign up action
+  handleSignUp({ email, password, username, phone }) {
+    this.props.signupUser({ email, password, username, phone }, this.props.history);
   }
 
   render() {
     return (
       <div className="payment-form-container1">
-        <WrappedRegisterUserForm />
+        <WrappedRegisterUserForm handleSubmit={this.handleSignUp} />
       </div>
     );
   }
 }
 
-RegisterUserContainer.propTypes = {
-
-};
-
-export default withRouter(connect(null, {})(RegisterUserContainer));
+// connects particular parts of redux state to this components props
+const mapStateToProps = state => (
+  {
+    authenticated: state.authenticated,
+  }
+);
+// react-redux glue -- outputs Container that know state in props
+// new way to connect with react router 4
+export default withRouter(connect(mapStateToProps, { signupUser })(RegisterUserContainer));
