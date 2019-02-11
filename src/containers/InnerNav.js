784b12ed52b28cb05ef8ac15e7e85d4041a719df
Joe Connolly <jcjconnolly4@gmail.com>
2019-02-10 23:57:28 -0500

    Added code for a rough inner nav

---
+++ src/containers/InnerNav.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, NavLink } from 'react-router-dom';
+import { signoutUser } from '../actions';
+import { getUser } from '../actions/userActions';
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.getAuthButtons = this.getAuthButtons.bind(this);
+  }
+
+  componentWillMount() {
+    if (this.props.authenticated.authenticated && !this.props.user) { this.props.getUser(); }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.authenticated.authenticated && !nextProps.user) { this.props.getUser(); }
+  }
+  getAuthButtons(event) {
+    if (this.props.authenticated.authenticated === false) {
+      return (
+        <div>
+          <NavLink to="/register" className="menu_item" id="login">Sign Up</NavLink>
+          <NavLink to="/login" className="menu_item" id="signup">Log In</NavLink>
+        </div>
+      );
+    } else {
+      /*eslint-disable */
+      return (
+        <div>
+          <NavLink to="/dashboard" className="menu_item" id="dashboard">Dashboard</NavLink>
+          <span className="menu_item" id="login" onClick={this.handleLogout}>Logout</span>
+        </div>
+      );
+      /*eslint-enable */
+    }
+  }
+
+  handleLogout(event) {
+    this.props.signoutUser(this.props.history, () => {
+      window.location.reload();
+    });
+  }
+
+
+  render() {
+    return (
+      <div style={{ backgroundColor: this.props.color }}id="nav">
+        {this.getAuthButtons()}
+        <NavLink to="/settings" className="menu_item" id="settings_button">Settings</NavLink>
+        <NavLink to="/">
+          <img src="https://i.imgur.com/TN4nDUA.png" alt="logo" id="logo" />
+        </NavLink>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => (
+  {
+    authenticated: state.authenticated,
+  }
+);
+
+// react-redux glue -- outputs Container that know state in props
+// new way to connect with react router 4
+export default withRouter(connect(mapStateToProps, { signoutUser, getUser })(Nav));
